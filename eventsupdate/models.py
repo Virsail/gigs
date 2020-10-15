ae7aeeff2bc2f94ab61be6c3a0f1f15cb763684b
virsail <virsaileric@gmail.com>
2020-10-15 12:45:28 +0300

    import pyp uploadcare module

--- eventsupdate/models.py
+++ eventsupdate/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from pyuploadcare.dj.models import ImageField
+from pyuploadcare.dj.forms import FileWidget
+
 
 # Create your models here.
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(255),unique = True,nullable = False)
+    email  = db.Column(db.String(255),unique = True,nullable = False)
+    secure_password = db.Column(db.String(255),nullable = False)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pitches = db.relationship('Pitch', backref='user', lazy='dynamic')
+    comment = db.relationship('Comment', backref='user', lazy='dynamic')
+    upvote = db.relationship('Upvote',backref='user',lazy='dynamic')
+    downvote = db.relationship('Downvote',backref='user',lazy='dynamic')
+    
+
+    @property
+    def set_password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @set_password.setter
+    def password(self, password):
+        self.secure_password = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.secure_password,password) 
+    
+    def save_u(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    
+    def __repr__(self):
+        return f'User {self.username}'
+
--- eventsupdate/views.py
+++ eventsupdate/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
-from .models import Image, Location
 
 # Create your views here.
+from .models import Image, Location
