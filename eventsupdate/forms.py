f201f67ea28530ee2edf2dad865c85328bb52eb3
virsail <virsaileric@gmail.com>
2020-10-15 15:29:35 +0300

    create user creation form in forms.py

--- eventsupdate/forms.py
+++ eventsupdate/forms.py
@@ -1,1 +1,16 @@
 from django.forms import ModelForm
+from .models import Event 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib .auth.models import User
+from django import forms
+class EventForm(ModelForm):
+    class Meta:
+        model = Event
+        fields = '__all__'
+
+
+
+
+
+
+class CreateUserForm( UserCreationForm):
--- eventsupdate/urls.py
+++ eventsupdate/urls.py
@@ -1,1 +1,3 @@
 from django.conf.urls import url
+from django.urls import path 
+from .import views
