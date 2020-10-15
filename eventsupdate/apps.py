03e6c60ada739cc4fb2afe75a31eb92e8899d0a8
virsail <virsaileric@gmail.com>
2020-10-15 10:55:40 +0300

    set application configuration in .env file

---
+++ eventsupdate/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EventsupdateConfig(AppConfig):
+    name = 'eventsupdate'
---
+++ eventsupdate/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+from .models import Image, Location
+
+# Create your views here.
