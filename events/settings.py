9a7417f8f8d9bbf02ea6e7b5c63a8e27f8ea8d75
virsail <virsaileric@gmail.com>
2020-10-17 16:17:07 +0300

    style the login form

--- events/settings.py
+++ events/settings.py
@@ -109,15 +109,6 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-    }
-}
 
 
 # Password validation
