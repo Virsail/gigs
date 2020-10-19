69f776b24310593f8b6c0276cd8335e29b6f9647
virsail <virsaileric@gmail.com>
2020-10-19 22:36:58 +0300

    add ajax message news letter

--- events/settings.py
+++ events/settings.py
@@ -165,3 +165,10 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 LOGIN_REDIRECT_URL='/'
+
+# Email configurations remember to install python-decouple
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
