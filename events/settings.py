c3425f1925bbc37b36f47ebc8281d60e0c9e72b3
virsail <virsaileric@gmail.com>
2020-10-23 16:34:11 +0300

    add background image on events section

--- events/settings.py
+++ events/settings.py
@@ -179,5 +179,4 @@ EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 #configs for sendgrid api 
-SENDGRID_API_KEY= config('SENDGRID_API_KEY')
 
